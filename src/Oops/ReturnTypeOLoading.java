b092e68f915954756ab611b8b8ff318624b68d9d
vijay <vijaypopuri2021@gmail.com>
2024-10-06 12:42:20 +0530

    returnTypeChngs

---
+++ src/Oops/ReturnTypeOLoading.java
@@ -0,0 +1,20 @@
+package Oops;
+class Add{
+	void add(int a,int b,int c) {
+		System.out.println("The adition is "+(a+b+c));
+	}
+	int add (int a,int b) {
+		return a+b;
+	}
+}
+public class ReturnTypeOLoading {
+
+	public static void main(String[] args)
+	{
+		
+		Add ad=new Add();
+		int res=ad.add(5, 9);
+		System.out.println(res);
+	}
+
+}
